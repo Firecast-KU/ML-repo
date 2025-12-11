7b1ff5d2e03879a1284659a536b4c7d04e7e4d29
dasol <pds2023@gmail.com>
2025-12-12 00:38:46 +0900

    feat: 산불 데이터 정규화 호출

---
+++ src/pipelines/build_fire_events.py
@@ -0,0 +1,10 @@
+# src/pipelines/build_fire_events.py
+from src.core.fire_events import normalize_fire_events
+
+
+def main():
+    normalize_fire_events()
+
+
+if __name__ == "__main__":
+    main()
